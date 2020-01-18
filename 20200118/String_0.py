e97d27321a1e1c4b87834c1a28b9d1d81b7cceaf
BestOfQ <3434614710@qq.com>
2020-01-18 22:20:56 +0800

    字符串学习——1
    字符串拼接、换行符的使用等

---
+++ 20200118/String_0.py
@@ -0,0 +1,22 @@
+# 将你的名字赋予给变量name，并打印出该变量
+name = 'tjq'
+print(name)
+# 他说：“我爱你！”  将这句话打印出来
+str1 = '他说：“我爱你！”'
+print(str1)
+
+# "I love you!" He said. 将这句话用两种方法打印出来
+print('"I love you!" He said.')
+print("\"I love you!\" He said.")
+
+# 打印出 /\/\/\
+print('/\\/\\/\\')
+
+# 分两行打印出 I am Chinese.I love China.
+print('I am Chinese.\nI love China.')
+
+# 将两字符串变量拼接，打印出 hello guys
+word1 = 'hello'
+word2 = 'guys'
+word = word1 + ' ' + word2
+print(word)
